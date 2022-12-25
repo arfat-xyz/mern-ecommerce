ae5e1170101bc6bbf83319f73f767002e4dd29b8
arfat-beep <arfatrahman08@gmail.com>
2022-12-26 00:56:54 +0600

    backend get single product added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,6 +33,21 @@ exports.updateProduct = async (req, res, next) => {
   });
 };
 
+// get single product details
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
 // delete Product
 exports.deleteProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
