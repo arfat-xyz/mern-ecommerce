ae43fd200e47373beea698a68200b82947d5abad
arfat-beep <arfatrahman08@gmail.com>
2022-12-26 00:51:51 +0600

    backend delete product added

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -3,9 +3,11 @@ const {
   getAllProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const router = express.Router();
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").delete(deleteProduct);
 module.exports = router;
