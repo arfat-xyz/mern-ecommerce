54454b9030d14560f8cd820c53ceae312aa61bfb
arfat-beep <arfatrahman08@gmail.com>
2022-12-27 13:19:48 +0600

    Backend registration errro message added

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT errro
+  if (err.code === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong Expire errro
+  if (err.code === "TokenExpireError") {
+    const message = `Json Web Token is expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(res.statusCode).json({
     success: false,
     error: err,
